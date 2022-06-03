91de7bcc2083cabfbdfe98403278ec83999d6046
Mykhaylo Marfeychuk <mykhamarf17@gmail.com>
2022-06-03 16:16:11 +0100

    Implement correct window and background rendering

--- src/ppu.cpp
+++ src/ppu.cpp
@@ -1,27 +1,51 @@
 #include <iostream>
 
-#include "gpu.h"
+#include "ppu.h"
 
-// Remove this later
-// #include <SDL2/SDL_image.h>
-// #include <SDL2/SDL_timer.h>
-
-
-GPU::GPU(Registers *registers, Interrupts* interrupts, MMU* mmu){
+PPU::PPU(Registers *registers, Interrupts* interrupts, MMU* mmu){
     this->registers = registers;
     this->mmu = mmu;
     this->interrupts = interrupts; 
-    control = &mmu->memory[0xff40];
+    // TODO: Rewrite this to use mmu->read_byte(0xff40)
+    control = (Control*)&mmu->memory[0xff40];
     scrollY = &mmu->memory[0xff42];
     scrollX = &mmu->memory[0xff43];
     scanline = &mmu->memory[0xff44];
 }
 
-void GPU::step(){
+void PPU::step(){
+
+    for (mmu->timer.div += mmu->clock.t - mmu->clock.t_prev; mmu->clock.t_prev++ != mmu->clock.t;)
+      if (control->lcdEnable) {
+        if (++modeclock == 456) {
+
+          if (*scanline < 144)
+            render_scan_lines();
+          if (*scanline == 143) {
+            this->interrupts->set_interrupt_flag(INTERRUPT_VBLANK);
+            can_render = true;
+            mmu->renderFlag.viewport = true;
+          }
+
+            if(*this->scanline == mmu->read_byte(0xff45)){// && *this->scanline > 0){
+                mmu->write_byte(0xff41, mmu->read_byte(0xff41) | (1 << 2));
+            }else{
+                mmu->write_byte(0xff41, mmu->read_byte(0xff41) & ~(1 << 2));
+            }
+          *scanline = (*scanline + 1) % 154;
+          modeclock = 0;
+        }
+    } else{
+        *scanline = modeclock = 0;
+    }
+}
+
+void PPU::step2(){
     // std::cout << "SCANLINE: " << +this->scanline << std::endl;
     // std::cout << "MODE " << mode << std::endl;
     // std::cout << "Incorrect cycles: " << this->cpu->cycles << std::endl;
-    modeclock += registers->t;
+    modeclock += mmu->clock.t;
+    // modeclock += registers->t;
     // if(!this->control){
     //     return;
     // }
@@ -33,13 +57,22 @@ void GPU::step(){
                 if(*this->scanline == 143){
                     if(this->interrupts->is_interrupt_enabled(INTERRUPT_VBLANK)){
                         this->interrupts->set_interrupt_flag(INTERRUPT_VBLANK);
+                        can_render = true;
                     }
+                    can_render = true;
                     mode = 1;
                 }else{
                     mode = 2;
                 }
                 modeclock -= 204;
                 // render_scan_lines();
+
+                // std::cout << "SCANLINE: "<< +(*this->scanline) << std::endl;
+                // if(*this->scanline == mmu->read_byte(0xff45) && *this->scanline > 0){
+                //     mmu->write_byte(0xFF41, mmu->read_byte(0xFF41) | (1 << 2));
+                // }else{
+                //     mmu->write_byte(0xFF41, mmu->read_byte(0xFF41) & ~(1 << 2));
+                // }
             }
             break;
         case 1: // VBLANK
@@ -52,9 +85,11 @@ void GPU::step(){
                     *this->scanline = 0;
                     mode = 2;
                     mmu->renderFlag.viewport = true;
-
-
-
+                }
+                if(*this->scanline == mmu->read_byte(0xff45) && *this->scanline > 0){
+                    mmu->write_byte(0xff41, mmu->read_byte(0xff41) | (1 << 2));
+                }else{
+                    mmu->write_byte(0xff41, mmu->read_byte(0xff41) & ~(1 << 2));
                 }
                 modeclock -=456;
             }
@@ -77,23 +112,21 @@ void GPU::step(){
     }
 }
 
-void GPU::render_scan_lines(){
-    bool scanlineRow[160];
-    int rowPixel = 0;
+void PPU::render_scan_lines(){
+    if(!this->control->lcdEnable)
+        return;
 
-    this->render_scan_line_background(scanlineRow);
-    this->render_scan_line_window(scanlineRow);
-    this->render_scan_line_sprites(scanlineRow);
+    this->render_scan_line_background();
+    this->render_scan_line_window();
+    this->render_scan_line_sprites();
 }
 
-void GPU::render_scan_line_background(bool scanlineRow[]){
-    int rowPixel = 0;
-
-    // Tiles
+void PPU::render_scan_line_background(){
     uint16_t address = 0x9800;
-    // if((*this->control >> 3) & 1)
-    //     address += 0x400;
-    
+
+    if(this->control->bgDisplaySelect)
+        address += 0x400;
+
     address += (*this->scanline / 8) * 32;
     address += (*this->scrollX >> 3);
 
@@ -102,42 +135,32 @@ void GPU::render_scan_line_background(bool scanlineRow[]){
     int pixelOffset = *this->scanline * 160;
 
     for(uint16_t tile_address = address; tile_address < address + 20; tile_address++){
-        // if(tile_address % 0x20 != 0){
-        //     exit(1);
-        // }
         int tile = this->mmu->read_byte(tile_address);
 
-        // Check if background tiles
-        // if(!(*this->control >> 5 & 0x1) && tile < 128)
-        //     tile += 256;
         if(tile < 128)
             tile += 256;
 
         for(; x < 8; x++){
             int colour = mmu->tiles[tile][y][x];
             framebuffer[pixelOffset++] = palette[colour];
-            scanlineRow[rowPixel++] = 1;
         }
         x=0;
     }
 }
 
-void GPU::render_scan_line_window(bool scanlineRow[]){
-    if(!(*this->control >> 5 & 0x1)){
+void PPU::render_scan_line_window(){
+    if(!this->control->windowEnable){
         return;
     }
+
     if(mmu->read_byte(0xFF4A) > *this->scanline){
         return; 
     }
-    // bool scanlineRow[160];
-    int rowPixel = 0;
 
-    // Tiles
-    uint16_t address = 0x9C00;
-    // if((*this->control >> 3) & 1)
-    //     address += 0x400;
+    uint16_t address = 0x9800;
+    if(this->control->windowDisplaySelect)
+        address += 0x400;
     
-    std::cout << +*this->scanline << std::endl;
     address += ((*this->scanline - mmu->read_byte(0xFF4A)) / 8) * 32;
     int y = (*this->scanline - mmu->read_byte(0xFF4A)) & 7;
     int x = 0;
@@ -148,48 +171,39 @@ void GPU::render_scan_line_window(bool scanlineRow[]){
     for(uint16_t tile_address = address; tile_address < address + 20; tile_address++){
         int tile = this->mmu->read_byte(tile_address);
 
-        // Check if background tiles
-        // if(!(*this->control >> 5 & 0x1) && tile < 128)
-        //     tile += 256;
         if(tile < 128)
             tile += 256;
 
         for(; x < 8; x++){
             int colour = mmu->tiles[tile][y][x];
             framebuffer[pixelOffset++] = palette[colour];
-            scanlineRow[rowPixel++] = 1;
         }
         x=0;
     }
 
 }
 
-void GPU::render_scan_line_sprites(bool scanlineRow[]){
-    int rowPixel = 0;
-    // Sprites
-    if(!((*control >> 1) & 1)) 
+void PPU::render_scan_line_sprites(){
+    if(!this->control->spriteDisplayEnable) 
         return;
 
     for(auto sprite : mmu->sprites){
         if(sprite.y <= *scanline && (sprite.y + 8) > *scanline) {
             int pixelOffset = *this->scanline * 160 + sprite.x;
-
+            
+            // Flip vertically
             uint8_t y = *scanline % sprite.y;
             if(sprite.options.yFlip) y = 7 - y;
 
             for(int x = 0; x < 8; x++){
                 if(sprite.x + x >= 0 && sprite.x + x < 160) {
-                    // if(!scanlineRow[sprite.x + x] || sprite.options.renderPriority)
-                    //     continue;
-
+                    // Flip horizontally
                     uint8_t xF = sprite.options.xFlip ? 7 - x : x;
                     uint8_t colour = mmu->tiles[sprite.tile][y][xF];
 
-                    if(colour){
+                    if(colour)
                         framebuffer[pixelOffset] = palette[colour];
-                    }
                     pixelOffset++;
-                    scanlineRow[sprite.x + x] = 1;
                 }
             }
         }
---
+++ src/ppu.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "Registers.h"
+#include "mmu.h"
+#include "interrupt.h"
+
+struct rgb {
+    unsigned char r, g, b, a;
+};
+typedef struct rgb COLOUR;
+
+const COLOUR palette[4] = {
+        { 255, 255, 255, 255},
+        // { 0, 192, 192, 192 },
+        // { 0, 96, 96, 96 },
+        // { 0, 255, 255, 255 },
+        // { 0, 255, 255, 255 },
+        { 192,192,192,255},
+        { 96,96,96, 255 },
+        { 0, 0, 0, 255 },
+    };
+
+
+class PPU{
+    public:
+        Registers* registers;
+        MMU* mmu;
+        Interrupts *interrupts;
+        uint8_t* scrollX;
+        uint8_t* scrollY;
+        uint8_t* scanline;
+        unsigned long tick;
+
+        struct Control
+        {
+            union
+            {
+                struct {
+                    uint8_t bgDisplay : 1;
+                    uint8_t spriteDisplayEnable : 1;
+                    uint8_t spriteSize : 1;
+                    uint8_t bgDisplaySelect : 1;
+                    uint8_t bgWindowDataSelect : 1;
+                    uint8_t windowEnable : 1;
+                    uint8_t windowDisplaySelect : 1;
+                    uint8_t lcdEnable : 1;
+                };
+            };
+        }*control;
+
+        COLOUR framebuffer[160 * 144];
+        unsigned short background[2000];
+        // unsigned short background[32 * 32];
+
+        int mode = 0;
+        int modeclock = 32;
+        // int modeclock = 0;
+        
+        bool can_render = false;
+
+    PPU(Registers *registers, Interrupts* interrupts, MMU *mmu);
+    void step(); 
+    void step2(); 
+    void render_scan_lines(); 
+    void render_scan_line_background(); 
+    void render_scan_line_window(); 
+    void render_scan_line_sprites(); 
+};
